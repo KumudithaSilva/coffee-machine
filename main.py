3e4ed8fda9b88d98c653e3e4d4e3451b5a25812b
Kumuditha <kumudithasilva66@gmail.com>
2025-05-18 19:18:24 +0530

    Check transaction and Make Coffee

--- main.py
+++ main.py
@@ -40,25 +40,22 @@ coins = {
 cash = 0
 machineState = True
 
-# TODO : 6. Check transaction successful?
-# TODO : 7. Make Coffee.
-
 # TODO : 1. Print report of all coffee machine resources
 def resources_details(resource_details, money):
     return (f"===Detailed Report===\n"
             f"Water: {resource_details['water']}ml\n"
             f"Milk: {resource_details['milk']}ml\n"
             f"Coffee: {resource_details['coffee']}g\n"
-            f"Money: ${money}\n{'='*20}")
+            f"Money: ${money}\n{'='*20}\n")
 
 # TODO : 2. Check resources sufficient to make drink water
 def calculate_resources(userchoices):
     ingredients = MENU[userchoices]['ingredients']
     for item in ingredients:
         if resources[item] < ingredients[item]:
-            print(f"Sorry there is not enough {item}.")
+            print(f"Sorry there is not enough {item}.\n")
             return False
-        return True
+    return True
 
 # TODO : 5. Process coins.
 def process_coin():
@@ -66,22 +63,46 @@ def process_coin():
     for item  in coins:
         user_coin = int(input(f"How many {item}:"))
         total += user_coin * coins[item]
-    return f"Total is {total}"
+    print(f"\nTotal is ${total}")
+    return total
 
+# TODO : 6. Check transaction successful?
+def transaction(user_total, coffe_cost, cash_amount):
+    global cash
+    if coffe_cost > user_total:
+        print("Sorry that's not enough money. Money refunded.\n")
+        return False
+    else:
+        if user_total >= coffe_cost:
+            print(f"Here is ${round(user_total-coffe_cost, 2)} dollars in change.\n")
+        cash = cash_amount + coffe_cost
+        return True
+
+# TODO : 7. Make Coffee.
+def make_coffe(userchoices):
+    ingredients = MENU[userchoices]['ingredients']
+    for item in ingredients:
+        resources[item] -= ingredients[item]
 
 # TODO : 3. Prompt user by asking “ What would you like? (espresso/latte/cappuccino):
-userinput = input("What would you like? (espresso/latte/cappuccino):")
-
 # TODO : 4. Turn off the Coffee Machine by entering “ off ” to the prompt.
-if userinput == "off":
-    machineState = False
-elif userinput == "report":
+print(resources_details(resources, cash))
+while machineState:
+    userinput = input("What would you like? (espresso/latte/cappuccino):")
+    if userinput == "off":
+        machineState = False
+    elif userinput == "report":
+        print(resources_details(resources, cash))
+    elif userinput in  {"espresso", "latte", "cappuccino" }:
+        if calculate_resources(userinput):
+            coffe_price = MENU[userinput]['cost']
+            print(f"\nTotal amount for the {userinput} is ${coffe_price}")
+            user_amount = process_coin()
+            if transaction(user_amount, coffe_price, cash):
+                make_coffe(userinput)
+    else:
+        print("Invalid input. Please choose correct commands.")
     print(resources_details(resources, cash))
-elif userinput in  {"espresso", "latte", "cappuccino" }:
-    if calculate_resources(userinput):
-        print(process_coin())
-else:
-    print("Invalid input. Please choose correct commands.")
 
 
 
